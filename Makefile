2044407614b5a7b7c49e6562640a2a40005dd6b8
Marc-Oliver Teschke <marc@Marc-Olivers-MacBook-Pro.local>
2013-05-15 21:01:06 +0200

    Added a gallery. Updated jQuery. Added htaccess redirect to non-www.

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 install:
 	rsync -rCuP --delete htdocs/* marcmachttheater.de@marcmachttheater.de:htdocs/
+	rsync -rCuP --delete htdocs/.htaccess marcmachttheater.de@marcmachttheater.de:htdocs/
 
 local:
 	rsync -aP --delete htdocs/ /Applications/MAMP/htdocs/marcmachttheater
--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -1,11 +1,4 @@
-/* Author: Marc-Oliver Teschke
-
-*/
-
 $(document).ready(function(){
-	$('.navigation a').click(function(){
-		$('html, body').animate({scrollTop:$('[id="'+$(this).attr("href").substr(1)+'"]').offset().top /*- ($('#back_to_top').outerHeight() * 1)*/}, 'slow');
-		return false;
-	});
+	$('.swipebox').swipebox();
 });
 
