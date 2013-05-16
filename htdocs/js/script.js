374bd8cd4fa1f853e8f43882a387d5399a7b8f04
Marc-Oliver Teschke <marc@Marc-Olivers-MacBook-Pro.local>
2013-05-16 08:03:20 +0200

    Scaled the images. Fiddled with caching.

--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+	preload_images();
 	$('.swipebox').swipebox();
 });
 
+
+function preload_images()
+{
+	var images = Array();
+	$('.gallery a').each(function(i, e){
+		images[i] = new Image();
+		images[i].src = $(e).attr('href');
+	});
+}
